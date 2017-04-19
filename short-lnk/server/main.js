32f92a4d3632d53b0aca11c02bc2d0a3c56536df
Christian Ringwald <mus3n@gmx.de>
2017-04-19 21:08:55 +0200

    Refactored the code in main.js client and server side; created imports/api/users.js and imports/routes/routes.js for that purpose

--- short-lnk/server/main.js
+++ short-lnk/server/main.js
@@ -1,57 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import SimpleSchema from 'simpl-schema';
-
-
+import '../imports/api/users';
 
 Meteor.startup(() => {
 
-  Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
-
-    try{
-      new SimpleSchema({
-        email:{
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({email});
-    } catch(e){
-      throw new Meteor.Error(400,e.message);
-    }
-
-    return true;
-
-  });
-
-
-
-  /*
-  // code to run on server at startup
-
-  const employeeSchema = new SimpleSchema({
-    name: {
-      type: String,
-      min: 1,
-      max: 200,
-    },
-
-    hourlyWage:{
-      type: Number,
-      min: 0
-    },
-
-    email:{
-        type: String,
-        optional: true,
-        regEx: SimpleSchema.RegEx.Email
-    }
-  });
-
-  employeeSchema.validate({
-    name: 'Chris',
-    hourlyWage: 5,
-    email: 'mus3n@gmx.de'
-  });
-*/
 });
